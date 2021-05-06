4625e53b77dbe2c6c7273726fe03b905d1342d66
Sheikh Bayazid <sheikhbayazid@gmail.com>
2021-05-06 18:07:48 +0600

    First Commit

--- GitDemo/ContentView.swift
+++ GitDemo/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPresented = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Button(action: {
+                    isPresented.toggle()
+                }, label: {
+                    Text("Profile")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .font(.system(size: 16, weight: .bold))
+                        .background(LinearGradient(gradient: Gradient (colors: [Color(UIColor(red: 0.98, green: 0.19, blue: 0.00, alpha: 1.00)), Color(UIColor(red: 0.98, green: 0.19, blue: 0.00, alpha: 1.00))]), startPoint: .leading, endPoint: .trailing))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                    
+                })
+            }.navigationTitle("Git Example")
+            .sheet(isPresented: $isPresented, content: {
+                NavigationView {
+                    Text("").navigationTitle("Sheet")
+                }
+            })
+        }
     }
 }
 
