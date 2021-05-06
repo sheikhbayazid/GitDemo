81f70ebe9f2f7e6c6333dd90bb9c442b529f3510
Sheikh Bayazid <sheikhbayazid@gmail.com>
2021-05-06 17:59:56 +0600

    Initial Commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Sheikh Bayazid on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
